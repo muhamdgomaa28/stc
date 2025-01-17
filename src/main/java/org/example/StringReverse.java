47e7fc7601b410cbc0119e9e09047759a15d0aac
Mohamed Gomaa <mohamedgomaa@catrion.com>
2025-01-17 23:03:16 +0200

    reverse substring

--- src/main/java/org/example/StringReverse.java
+++ src/main/java/org/example/StringReverse.java
@@ -1,4 +1,33 @@
 package org.example;
 
+import java.util.Stack;
+
 public class StringReverse {
+
+    public static String reverseSupStrings(String s) {
+        StringBuilder current = new StringBuilder();
+        Stack<StringBuilder> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(current);
+                current = new StringBuilder();
+            } else if (c == ')') {
+                 current.reverse();
+                current = stack.pop().append('(').append(current).append(')');
+
+            } else {
+                current.append(c);
+            }
+        }
+
+        return current.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseSupStrings("abd(jnb)asdf(ab)"));
+        System.out.println(reverseSupStrings("dd(df)a(ghhh)"));
+
+
+    }
 }
